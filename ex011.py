d360567bf6affc121b05756157ce04f303727dd0
Tuca <arturalicia9@gmail.com>
2026-05-06 00:25:33 -0300

    exercícios 11 ao 13

---
+++ ex011.py
@@ -0,0 +1,6 @@
+l = float(input('Digite a largura da parede em metros: '))
+h = float(input('Digite a altura da parede em metros: '))
+a = l * h
+t = a / 2
+print(f'Sua parede tem a dimensão de {l} x {h} e sua área é de {a}m².')
+print(f'Para pintar essa parede, você precisará de {t}l de tinta.')
---
+++ ex013.py
@@ -0,0 +1,2 @@
+s = float(input('Digite o salário do funcionário: R$ '))
+print(f'Um funcionário que ganha R$ {s:.2f} com 15% de aumento passará a ganhar R$ {s*1.15:.2f}')
